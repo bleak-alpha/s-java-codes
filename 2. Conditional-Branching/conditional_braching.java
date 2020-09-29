3acd3bfaeaaaa55384150166f7b2d80907f6a137
Sankalp A. Thorat <71189549+bleak-alpha@users.noreply.github.com>
2020-09-29 16:09:26 +0530

    Create conditional_braching.java

---
+++ 2. Conditional-Branching/conditional_braching.java
@@ -0,0 +1,16 @@
+class IfTest{
+    public static void main(String[] args) {
+       int x=3;
+       if(x==3)
+          System.out.print("x must be 3");
+       System.out.print("\nThis runs no matter what");
+    }
+}
+class IfTest2{
+    public static void main(String[] args){
+      int x=2;
+      if(x==3)
+         System.out.print("x is not 3");
+      System.out.print("this runs no matter what");
+    }
+}
