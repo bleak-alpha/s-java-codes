8372d54825026843e34f6b9807bba5f8d6ecda5e
bleak-alpha <sankalpthorat85@hotmail.com>
2020-09-30 12:46:11 +0530

    commit

--- 4. Array/PhraseOMatic.java
+++ 4. Array/PhraseOMatic.java
@@ -26,7 +26,7 @@ public class PhraseOMatic{
 
         //Now build a phrase
         String phrase=wordListOne[rand1]+" "+wordListTwo[rand2]+" "
-                +wordListThree[rand3];
+                +wordListThree[rand3]+"\n";
 
         //Now print out the phrase
         System.out.print("What we need is a "+phrase);
